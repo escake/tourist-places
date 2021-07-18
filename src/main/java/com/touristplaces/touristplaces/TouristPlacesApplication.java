d6686f2ef84e905bc34d76c45f0b37d6aa9da141
MerjemH <merjem.hasanovic@tacta.io>
2021-07-18 16:02:39 +0200

    Initial commit

---
+++ src/main/java/com/touristplaces/touristplaces/TouristPlacesApplication.java
@@ -0,0 +1,13 @@
+package com.touristplaces.touristplaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristPlacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristPlacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/touristplaces/touristplaces/TouristPlacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.touristplaces.touristplaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristPlacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
